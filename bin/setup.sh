531e655117179f530e0b1a2e50bd4d003c49abc4
Adriano Calvitto <adriano.calvitto@gmail.com>
2017-11-05 15:51:39 +0100

    try linking themes in wp installation

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker-compose exec --user www-data phpfpm rm -rf web
 docker-compose exec --user www-data phpfpm composer install
 docker-compose exec --user www-data phpfpm cp -R src/wp-content/themes web/wp-content
 docker-compose exec --user www-data phpfpm mv vendor web/vendor
