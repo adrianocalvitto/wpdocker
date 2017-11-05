ca9927c59a1129d955eccb96d975c5cd22e06c6e
Adriano Calvitto <adriano.calvitto@gmail.com>
2017-11-05 14:46:44 +0100

    using env vars in wp-config

--- public/src/wp-config.php
+++ public/src/wp-config.php
@@ -1,38 +1,32 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *	
- * @package WordPress
- */
+$root_dir = dirname(__DIR__);
+$webroot_dir = $root_dir . '/web';
+require_once($webroot_dir . '/vendor/autoload.php');
+/* Expose global env() function from oscarotero/env */
+Env::init();
+/* Use Dotenv to set required environment variables and load .env file in root */
+$dotenv = new Dotenv\Dotenv($webroot_dir);
+if (file_exists($webroot_dir . '/.env')) {
+    $dotenv->load();
+    $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
+}
+/* Set up our global environment constant and load its config first */
+define('WP_ENV', env('WP_ENV') ?: 'production');
 /**
  * URLs
  */
-define('WP_HOME', 'http://dev.catchys.de');
-define('WP_SITEURL', 'http://dev.catchys.de');
+define('WP_HOME', env('WP_HOME'));
+define('WP_SITEURL', env('WP_SITEURL'));
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', env('DB_NAME'));
 /** MySQL database username */
-define('DB_USER', 'wordpress');
+define('DB_USER', env('DB_USER'));
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', env('DB_PASSWORD'));
 /** MySQL hostname */
-define('DB_HOST', 'mysql');
-/** Database Charset to use in creating $root_dir = dirname(__DIR__);
-$webroot_dir = $root_dir . '/web';database tables. */
+define('DB_HOST', env('DB_HOST'));
+/** Database Charset to use */
 define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -45,14 +39,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'l !nIS?]WejZ-3cp<71JKWAs;79Z!Xw,2Qe)w>,rc3rD#`1*tUN@;iU;{aWEY?q}');
-define('SECURE_AUTH_KEY',  '@8G)k`a7:Q<3}Qr#NpP7@4_)hO!j}AM(9*<!J0nJ>!jJc;r?:zD43C$TNaB<z*P`');
-define('LOGGED_IN_KEY',    'IwmKogEzUhjiH;~rEvWS(*Qy3.)`QLXKGiMD.!q[H>4<dUJpwKqYI@Y8H/hUs5J-');
-define('NONCE_KEY',        '_+?1kEU)Two~MDm9;pY 2uVBIah;40$Lfx1FPD;uy(#<tr&11RTCEy/Pw42`(]<%');
-define('AUTH_SALT',        'cSfKINnT?[]Vdd;o~1Fa*g+DaK0JgsI,D};b1s|#jt3ap+](dG+-hVqiJJmVv)ZL');
-define('SECURE_AUTH_SALT', 'n{Gj,b:fiFyZ3J/JRY_UzI(^oS:cE`P}J/6)QNL-^9(XvdclQg<r,t.lwpYHB&lA');
-define('LOGGED_IN_SALT',   'B2B>@DB_U:9Oy_##I0 %s1w(%B;I2#cQ(nv6g[HP<+~8=E=YB!NS~ya^n)VHl0jR');
-define('NONCE_SALT',       '*9L&^2h4L5(_=z=M/MuiUAw<S9Oo:dTAtsGz/*/.dE7}mjm ;sCj7LR7SBwS@p{o');
+define('AUTH_KEY', env('AUTH_KEY'));
+define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
+define('NONCE_KEY', env('NONCE_KEY'));
+define('AUTH_SALT', env('AUTH_SALT'));
+define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
+define('NONCE_SALT', env('NONCE_SALT'));
 /**#@-*/
 /**
  * WordPress Database Table prefix.
@@ -60,7 +54,7 @@ define('NONCE_SALT',       '*9L&^2h4L5(_=z=M/MuiUAw<S9Oo:dTAtsGz/*/.dE7}mjm ;sCj
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = env('DB_PREFIX');
 /**
  * For developers: WordPress debugging mode.
  *
@@ -73,9 +67,9 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('SAVEQUERIES', true);
-define('WP_DEBUG', true);
-define('SCRIPT_DEBUG', true);
+define('SAVEQUERIES', env('SAVEQUERIES'));
+define('WP_DEBUG', env('WP_DEBUG'));
+define('SCRIPT_DEBUG', env('SCRIPT_DEBUG'));
 /* That's all, stop editing! Happy blogging. */
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
