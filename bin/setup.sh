9f8eb6c3bcf805a134c76e2e1eb4a5679cfaa8c6
Adriano Calvitto <adriano.calvitto@gmail.com>
2017-11-05 19:07:55 +0100

    removed rendundant closing tag

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,4 +2,4 @@
 docker-compose exec --user www-data phpfpm rm -rf web
 docker-compose exec --user www-data phpfpm composer install
 docker-compose exec --user www-data phpfpm touch web/wp-config.php
-docker-compose exec --user www-data phpfpm bash -c "echo '<?php require_once(dirname(__DIR__) . \"/wp-config.php\"); ?>' >> /var/www/html/web/wp-config.php"
+docker-compose exec --user www-data phpfpm bash -c "echo '<?php require_once(dirname(__DIR__) . \"/wp-config.php\");' >> /var/www/html/web/wp-config.php"
