cc096b803bafd10434c97132cddce8e8d6fa48c9
Adriano Calvitto <adriano.calvitto@gmail.com>
2018-07-15 21:32:35 +0100

    install twentysixteen by default, leave choice to the user

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,4 +5,4 @@ docker-compose exec --user www-data phpfpm touch web/wp-config.php
 docker-compose exec --user www-data phpfpm bash -c "echo '<?php' >> /var/www/html/web/wp-config.php"
 docker-compose exec --user www-data phpfpm bash -c "echo 'require_once(dirname(__DIR__) . \"/wp-config.php\");' >> /var/www/html/web/wp-config.php"
 docker-compose exec --user www-data phpfpm bash -c "echo 'require_once(ABSPATH . \"wp-settings.php\");' >> /var/www/html/web/wp-config.php"
-docker-compose exec --user www-data phpfpm wp theme activate starter-theme
+docker-compose exec --user www-data phpfpm wp theme install twentysixteen --activate
