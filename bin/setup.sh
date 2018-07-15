ae88b50a21ddc498c3bedb66285b795482195f4f
Adriano Calvitto <adriano.calvitto@gmail.com>
2018-07-15 21:17:33 +0100

    setup default theme on setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,3 +5,4 @@ docker-compose exec --user www-data phpfpm touch web/wp-config.php
 docker-compose exec --user www-data phpfpm bash -c "echo '<?php' >> /var/www/html/web/wp-config.php"
 docker-compose exec --user www-data phpfpm bash -c "echo 'require_once(dirname(__DIR__) . \"/wp-config.php\");' >> /var/www/html/web/wp-config.php"
 docker-compose exec --user www-data phpfpm bash -c "echo 'require_once(ABSPATH . \"wp-settings.php\");' >> /var/www/html/web/wp-config.php"
+docker-compose exec --user www-data phpfpm wp theme activate starter-theme
