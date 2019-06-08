69f10ed6b55d12b996361833ac0bd2723da3b4fd
Adriano Calvitto <adriano.calvitto@gmail.com>
2019-06-08 21:34:35 +0100

    vendor and wp installation folder are created by root

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-docker-compose exec --user www-data phpfpm rm -rf web
-docker-compose exec --user www-data phpfpm composer install
-docker-compose exec --user www-data phpfpm touch web/wp-config.php
+
+docker-compose exec --user root phpfpm rm -rf web
+docker-compose exec --user root phpfpm composer install
+docker-compose exec --user root phpfpm touch web/wp-config.php
 # Create config file and include parent config (security reasons)
-docker-compose exec --user www-data phpfpm bash -c "echo '<?php' >> /var/www/html/web/wp-config.php"
-docker-compose exec --user www-data phpfpm bash -c "echo 'require_once(dirname(__DIR__) . \"/wp-config.php\");' >> /var/www/html/web/wp-config.php"
-docker-compose exec --user www-data phpfpm bash -c "echo 'require_once(ABSPATH . \"wp-settings.php\");' >> /var/www/html/web/wp-config.php"
+docker-compose exec --user root phpfpm bash -c "echo '<?php' >> /var/www/html/web/wp-config.php"
+docker-compose exec --user root phpfpm bash -c "echo 'require_once(dirname(__DIR__) . \"/wp-config.php\");' >> /var/www/html/web/wp-config.php"
+docker-compose exec --user root phpfpm bash -c "echo 'require_once(ABSPATH . \"wp-settings.php\");' >> /var/www/html/web/wp-config.php"
 # We want no description
 docker-compose exec --user www-data phpfpm wp option update blogdescription ""
 # Disable comments
